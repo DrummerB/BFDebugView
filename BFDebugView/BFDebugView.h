ea6bc20160e3620165dc4ac8ba8b6ed0cb7199fd
Balazs Faludi <balazsfaludi@gmail.com>
2014-03-17 15:53:18 +0100

    Initial Commit

---
+++ BFDebugView/BFDebugView.h
@@ -0,0 +1,13 @@
+//
+//  BFDebugView.h
+//  BFDebugView
+//
+//  Created by Balázs Faludi on 17.03.14.
+//  Copyright (c) 2014 Balazs Faludi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BFDebugView : NSObject
+
+@end
---
+++ BFDebugView/BFDebugView.m
@@ -0,0 +1,13 @@
+//
+//  BFDebugView.m
+//  BFDebugView
+//
+//  Created by Balázs Faludi on 17.03.14.
+//  Copyright (c) 2014 Balazs Faludi. All rights reserved.
+//
+
+#import "BFDebugView.h"
+
+@implementation BFDebugView
+
+@end
